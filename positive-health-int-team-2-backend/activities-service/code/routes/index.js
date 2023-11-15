50a232f83461973922abddd7dfacb8afdd563439
netas369 <neve0023@hz.nl>
2023-11-15 11:30:05 +0100

    added database for each ms in docker environment, and connection trough php my admin

--- positive-health-int-team-2-backend/activities-service/code/routes/index.js
+++ positive-health-int-team-2-backend/activities-service/code/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { testTheFunctionality } from '../controllers/activityController.js';
+import { getActivities } from '../controllers/activityController.js';
 const router = express.Router();
 
 // here define all the routes
@@ -8,6 +9,7 @@ router.get('/', (req, res, next) => {
   });
 
 router.get('/', testTheFunctionality);
+router.get('/activities', getActivities);
 // between
 
 export default router;
