340ae5b9ed51ba8da514bb5e921fe0b2889ed272
Damyan03 <112628176+Damyan03@users.noreply.github.com>
2023-11-17 02:18:36 +0100

    Start, Login and Regsiter screen front-end

---
+++ positive-health-int-team-2-frontend/src/routes/register/+page.svelte
@@ -0,0 +1,42 @@
+<div class="flex justify-between flex-col w-screen h-screen">
+    <div class="h-20 w-full"></div>          
+    <div class="w-full h-full center flex-col">          
+        <div class="w-full h-1/3 center">
+            <img src="/logo.png" alt="StepUp!_logo" class="h-2/3">
+        </div>
+        <div class="w-full h-2/3">
+            <div class="center h-10">
+                <div class="w-2/3 h-full center border-b border-orange-400">
+                    <input type="text" placeholder="Username" class="bg-transparent outline-none w-full">
+                </div>
+            </div>
+            <div class="center h-10">
+                <div class="w-2/3 h-full center border-b border-orange-400">
+                    <input type="text" placeholder="E-Mail" class="bg-transparent outline-none w-full">
+                </div>  
+            </div>    
+            <div class="center h-10">
+                <div class="w-2/3 h-full center border-b border-orange-400">
+                    <input type="text" placeholder="Password" class="bg-transparent outline-none w-full">
+                </div>  
+            </div>  
+            <div class="center h-10">
+                <div class="w-2/3 h-full center border-b border-orange-400">
+                    <input type="text" placeholder="Confirm Password" class="bg-transparent outline-none w-full">
+                </div>
+            </div>
+            <div class="w-full h-20 center flex justify-evenly mt-5">
+                <a href="/login" class="w-1/3 h-3/5">
+                    <div class="w-full h-full border border-orange-500 center rounded-3xl">
+                        <h2>Already a user?</h2>
+                    </div>
+                </a>
+                <a href="" class="w-1/3 h-3/5">
+                    <div class="w-full h-full border border-orange-500 center rounded-3xl">
+                        <h2>Join</h2>
+                    </div>
+                </a>
+            </div>    
+        </div>
+    </div>  
+</div>  
