2e492571071c6b5536c2d7fbe4ea76a321c533e9
Damyan03 <112628176+Damyan03@users.noreply.github.com>
2023-11-17 02:39:50 +0100

    bug fix

--- positive-health-int-team-2-frontend/src/routes/+layout.svelte
+++ positive-health-int-team-2-frontend/src/routes/+layout.svelte
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <script>
     import "../app.css";
   </script>
   
   <slot />
-=======
-<script>
-    import "../app.css";
-  </script>
-
-<slot />
->>>>>>> fe33ba27989e6c9fe5038edb279e0ff9f49e90b8
