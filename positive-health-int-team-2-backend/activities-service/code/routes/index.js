7a59fa20a65a08758b744282ee8ae8acba70bb58
netas369 <neve0023@hz.nl>
2023-11-20 14:12:14 +0100

    added https sertificates to both back end and front end

--- positive-health-int-team-2-backend/activities-service/code/routes/index.js
+++ positive-health-int-team-2-backend/activities-service/code/routes/index.js
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post('/start', startActivity);
 router.post('/update-location', updateLocation);
 router.post('/stop', stopActivity);
-
+router.get('/', (req, res, next) => {
+    res.json('Backend service is running!');
+  });
 export default router;
