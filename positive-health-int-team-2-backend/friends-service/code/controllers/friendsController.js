71bb94c4546d5073f84884b003fb537fe1ecf196
Bartosz Adamczyk <adam0016@hz.nl>
2023-12-16 22:19:09 +0100

    Update the code before rebasing

--- positive-health-int-team-2-backend/friends-service/code/controllers/friendsController.js
+++ positive-health-int-team-2-backend/friends-service/code/controllers/friendsController.js
@@ -1,13 +1,16 @@
+import { Op } from "sequelize";
+import Friendship from "../models/Friendship.js";
+
 async function findPerson(req,res){
   try{
-    const response = await fetch('http://api-gateway:3025/auth/user/'+req.params.prompt, {
+    const responsePeople = await fetch('http://api-gateway:3025/auth/user/'+req.params.prompt, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
       }
     });
-    const result = await response.json();
+    let result = await responsePeople.json();
     if(!response.ok) throw "Not found!";
     return res.status(200).json(result);
   } catch(error){
@@ -15,8 +18,19 @@ async function findPerson(req,res){
   }
   }
 
-async function findRelationship(req,res){
-
+async function findRelationships(req,res){
+    const yourUserId = req.params.yourUserId;
+    let relationsArray = [];
+    let allRelations = await Friendship.findAll({where: {[Op.or]: [{friend_one: yourUserId}, {friend_two: yourUserId}]}});
+    allRelations.forEach(relation => {
+      if(relation.friend_two === yourUserId){
+        const swapVar = relation.friend_two
+        relation.friend_two = relation.friend_one;
+        relation.friend_one = swapVar;
+      }
+      relationsArray.push({friend: relation.friend_two, status: relation.status});
+    });
+    return res.status(200).json(relationsArray);
 }
 
-export default { findPerson, findRelationship }
+export default { findPerson, findRelationships }
--- positive-health-int-team-2-frontend/src/routes/app/friends/find/+page.svelte
+++ positive-health-int-team-2-frontend/src/routes/app/friends/find/+page.svelte
@@ -8,15 +8,33 @@
     async function searchUsers(){
         const prompt = document.getElementById('user').value;
         if(prompt.length === 0) throw new Error("Start typing sth...")
-        const usersFetch = await fetch('http://localhost:3025/friends/findperson/'+prompt, {
+        const usersFetch = await fetch('http://localhost:3021/friends/findperson/'+prompt, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
             }
         });
-        const foundRecords = await usersFetch.json();
-        if(usersFetch.ok) return foundRecords;
+        const foundUserRecords = await usersFetch.json();
+        const yourUserId = localStorage.getItem('userid');
+        const relationsFetch = await fetch('http://localhost:3021/friends/findrelationships/'+yourUserId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+        let foundRelationsFetch = await relationsFetch.json();
+        foundRelationsFetch = foundRelationsFetch.relationsArray;
+        foundUserRecords.users.forEach(user => {
+        if(foundRelationsFetch.includes({friend: user.id})){
+          if(foundRelationsFetch[foundRelationsFetch.indexOf({friend: user.id})].status === 1) user.friendStatus = "Pending request";
+          else if(foundRelationsFetch[foundRelationsFetch.indexOf({friend: user.id})].status === 2) user.friendStatus = "Friends";
+        }else{
+          user.friendStatus = "Strangers";
+        }
+    });
+        if(usersFetch.ok && relationsFetch.ok) return foundUserRecords;
         else throw new Error("Problems with connection.")
     }
 </script>
@@ -31,7 +49,7 @@
     {:else}
         {#if data.foundRecords.user !== null && data.foundRecords.users.length !== 0}
             <ul>
-                <li class="font-bold"> {data.foundRecords.user.username} | {data.foundRecords.user.email} </li>
+                <li class="font-bold"> {data.foundRecords.user.username} | {data.foundRecords.user.email} | {data.foundRecords.user.friendStatus} </li>
             </ul>
         {/if}
         {#if data.foundRecords.users.length === 0}
@@ -42,11 +60,11 @@
             {#each data.foundRecords.users as users}
                     {#if data.foundRecords.user === null}
                         <ul> 
-                            <li> {users.username} | {users.email}</li>
+                            <li> {users.username} | {users.email} | {data.foundRecords.user.friendStatus}</li>
                         </ul>
                     {:else if data.foundRecords.user.username !== users.username}
                         <ul> 
-                            <li> {users.username} | {users.email}</li>
+                            <li> {users.username} | {users.email} | {data.foundRecords.user.friendStatus}</li>
                         </ul>
                     {/if}
             {/each}
