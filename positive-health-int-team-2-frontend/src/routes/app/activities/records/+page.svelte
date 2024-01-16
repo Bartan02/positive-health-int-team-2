ab0cafcb2739e4983ee4ad4e4fbb09be43640fd6
Dominykas <vais0001@hz.nl>
2024-01-16 18:09:46 +0100

    Records page design

--- positive-health-int-team-2-frontend/src/routes/app/activities/records/+page.svelte
+++ positive-health-int-team-2-frontend/src/routes/app/activities/records/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte';
+    import TopMenu from '../../../../components/top-menu.svelte';
     let activities = [];
 
     onMount(async () => {
@@ -16,38 +17,46 @@
     });
 </script>
 
-
-<table>
-    <thead>
-        <tr>
-            <th>Activity ID</th>
-            <th>User ID</th>
-            <th>Start Time</th>
-            <th>Start Location</th>
-            <th>Distance</th>
-            <th>Last Location</th>
-            <th>Maximum Speed</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each activities as activity}
-            <tr>
-                <td>{activity.activity_id}</td>
-                <td>{activity.user_id}</td>
-                <td>{activity.start_time}</td>
-                <td>Lat: {activity.start_location.latitude}, Lon: {activity.start_location.longitude}</td>
-                <td>{activity.distance}</td>
-                <td>
-                    {#if activity.last_location}
-                        Lat: {activity.last_location.latitude}, Lon: {activity.last_location.longitude}
-                    {:else}
-                        N/A
-                    {/if}
-                </td>
-                <td>{activity.maximum_speed || 'N/A'}</td>
-            </tr>
-        {/each}
-    </tbody>
-</table>
-
-<h1>Distance</h1>
+<body>
+    <div class="min-h-screen" style="background: F6F7FB;">
+		<TopMenu menuLabel="Records" />
+        <!-- Frame for all components, setting the width to 90% of the viewport -->
+        <div style="padding-top: 146.6px;">
+        <div class="w-full mx-auto" style="width: 90%;">
+            <div class="overflow-x-auto">
+                <table class="w-full text-left table-auto">
+                    <thead class="text-sm text-gray-700 bg-gray-200">
+                        <tr>
+                            <th class="px-4 py-2">Activity ID</th>
+                            <th class="px-4 py-2">User ID</th>
+                            <th class="px-4 py-2">Start Time</th>
+                            <th class="px-4 py-2">Start Location</th>
+                            <th class="px-4 py-2">Distance</th>
+                            <th class="px-4 py-2">Last Location</th>
+                            <th class="px-4 py-2">Maximum Speed</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-xs sm:text-sm bg-white">
+                        {#each activities as activity}
+                            <tr class="border-b">
+                                <td class="px-4 py-2">{activity.activity_id}</td>
+                                <td class="px-4 py-2">{activity.user_id}</td>
+                                <td class="px-4 py-2">{activity.start_time}</td>
+                                <td class="px-4 py-2">Lat: {activity.start_location.latitude}, Lon: {activity.start_location.longitude}</td>
+                                <td class="px-4 py-2">{activity.distance}</td>
+                                <td class="px-4 py-2">
+                                    {#if activity.last_location}
+                                        Lat: {activity.last_location.latitude}, Lon: {activity.last_location.longitude}
+                                    {:else}
+                                        N/A
+                                    {/if}
+                                </td>
+                                <td class="px-4 py-2">{activity.maximum_speed || 'N/A'}</td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            </div>
+</div>
+</div>
+</body>
