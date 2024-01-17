1412dd282df3d4fa17e7d4694389b4f235fe34c8
Bartosz Adamczyk <adam0016@hz.nl>
2024-01-18 00:06:39 +0100

    Change database credentials for some microservices

--- positive-health-int-team-2-backend/friends-service/code/connection/connection.js
+++ positive-health-int-team-2-backend/friends-service/code/connection/connection.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('friends_db', 'example123', 'example123', {
-  host: 'friends-db',
+const sequelize = new Sequelize('d2b651m1hl6ph1lg', 'xv8vga9294v4i5xh', 'uy8d96wc6z85kb79', {
+  host: 'n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
   dialect: 'mysql',
 });
 
